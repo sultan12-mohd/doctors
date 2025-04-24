42d25a6af95e22e95ed93c5273b680c2ea938848
sultan12-mohd <sm620063@gmail.com>
2025-04-24 10:01:44 +0530

    first change

--- fronted/src/main.jsx
+++ fronted/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import AppContextProvider from './context/Appcontext.jsx'
+import AppContextProvider from './context/Appcontext.'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
